e3384bc640056981abea4e858e104ee2f3f7dba1
ZackYang <zackyang214@gmail.com>
2018-06-08 10:44:44 +1000

    Update 20161224120535_remove_environment_from_tracker.spree.rb

--- db/migrate/20161224120535_remove_environment_from_tracker.spree.rb
+++ db/migrate/20161224120535_remove_environment_from_tracker.spree.rb
@@ -1,3 +1,6 @@
+class Spree::Tracker < ApplicationRecord
+end
+
 # This migration comes from spree (originally 20150128032538)
 class RemoveEnvironmentFromTracker < ActiveRecord::Migration[4.2]
   def up
